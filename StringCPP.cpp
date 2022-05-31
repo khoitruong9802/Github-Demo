58844bc7367d8900eb98f9e35e4864bdd3038242
Trương Mạnh Khôi <khoitruong9802@gmail.com>
2022-05-31 19:54:52 +0700

    Previous Commit Wrong. Right commit: Test Char in cpp

--- StringCPP.cpp
+++ StringCPP.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 
 int main() {
-    std::string myName = "Truong Manh Khoi";
+    // std::string myName = "Truong Manh Khoi";
+    // std::cout << myName << std::endl;
+    // myName[11] = '\0';
+    // std::cout << myName << std::endl;
+    // return 0;
+    char myName[] = "Truong Manh Khoi";
     std::cout << myName << std::endl;
     myName[11] = '\0';
     std::cout << myName << std::endl;
