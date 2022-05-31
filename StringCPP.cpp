a52439a331e9ace9e6a2459382048c7101ffa828
Trương Mạnh Khôi <khoitruong9802@gmail.com>
2022-05-31 19:49:09 +0700

    Test string CPP

---
+++ StringCPP.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::string myName = "Truong Manh Khoi";
+    std::cout << myName << std::endl;
+    myName[11] = '\0';
+    std::cout << myName << std::endl;
+    return 0;
+}
