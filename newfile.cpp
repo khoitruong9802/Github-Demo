1dcabf165ffbd25bf0c58aa2095d1934e8ceae5f
Trương Mạnh Khôi <khoitruong9802@gmail.com>
2022-12-15 15:07:57 +0700

    int add(int a, int b)

--- newfile.cpp
+++ newfile.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
+int add(int a, int b) {
+    int c = a + b;
+    return c;
+}
+
 int main() {
     std::cout << "Hello world";
+    int test1, test2;
+    test1 = 100;
+    test2 = 124;
+    std::cout << add(test1, test2);
     return 0;
 }
